5fc7b1ff8ad459cd84a54857fa14a6b2acb1bf3f
Belvedere Alessandro <belvedere.alessandro@osdbsesto>
2022-02-16 09:34:41 +0100

    adding server and client socket mains

--- ClientSocket/src/clientsocket/ClientSocket.java
+++ ClientSocket/src/clientsocket/ClientSocket.java
@@ -5,6 +5,11 @@
  */
 package clientsocket;
 
+import java.net.*;
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author belvedere.alessandro
@@ -16,6 +21,22 @@ public class ClientSocket {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Socket clientSocket;
+        try {
+            clientSocket = new Socket("10.1.33.15", 5000);
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            out.println("hello server");
+            String resp = in.readLine();
+            System.out.println(resp);
+            in.close();
+            out.close();
+            clientSocket.close();
+
+        } catch (IOException ex) {
+            Logger.getLogger(ClientSocket.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
-    
+
 }
